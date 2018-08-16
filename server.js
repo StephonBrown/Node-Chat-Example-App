1c648542cda7890417129601452a3ceaa3907dec
Stephon Brown <stephonb14@gmail.com>
2018-08-16 00:41:39 -0400

    Changes to implement MongoDB

--- server.js
+++ server.js
@@ -3,28 +3,46 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+var dbUrl = ''
+
 
 app.use(express.static(__dirname))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-var messages = [
-    { name: 'Stephon', message: 'Yo!'},
-    { name: 'Tidney', message: 'Yep!'}
-]
-app.get('/messages', (req, res)=>{
-    res.send(messages)
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
+
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+    })
+})
+
 app.post('/messages', (req, res)=>{
-    io.emit('message', req.body)
-    messages.push(req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    message.save((err)=>{
+        if(err){
+            sendStatus(500)
+        }else{
+            io.emit('message', req.body)
+            res.sendStatus(200)
+        }
+    })
+
 })
 
 io.on('connection', (socket)=>{
     console.log("a user connected")
 })
 
+mongoose.connect(dbUrl,{ useNewUrlParser: true }, (err)=>{
+    console.log('mongo db connection', err)
+})
 var server = http.listen(3000, ()=>{
     console.log('server is listening on port', server.address().port)
 })
